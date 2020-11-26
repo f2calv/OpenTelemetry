95ba38ba12040cee218ce6e5dd3f0be3483f2a92
Alexander Vincent <16097639+f2calv@users.noreply.github.com>
2020-11-26 20:07:56 +0100

    added Jaeger exporter from samples

---
+++ README.md
@@ -0,0 +1,7 @@
+# OpenTelemetry
+
+Playground working with OpenTelemetry and various exporters....
+
+## Resources
+
+- https://github.com/open-telemetry/opentelemetry-dotnet
--- src/OpenTelemetryApp/Program.cs
+++ src/OpenTelemetryApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using OpenTelemetry;
+﻿using Examples.Console;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using System;
 using System.Diagnostics;
@@ -11,17 +13,39 @@ namespace CasCap
 
         public static void Main()
         {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .SetSampler(new AlwaysOnSampler())
-                .AddSource(sourceName)
-                .AddConsoleExporter()
-                .Build();
+            //using var openTelemetry = Sdk.CreateTracerProviderBuilder()
+            //    .SetSampler(new AlwaysOnSampler())
+            //    .AddSource(sourceName)
+            //    .AddConsoleExporter()
+            //    .Build();
 
-            using (var activity = MyActivitySource.StartActivity("SayHello"))
+            //using (var activity = MyActivitySource.StartActivity("SayHello"))
+            //{
+            //    activity?.SetTag("foo", 1);
+            //    activity?.SetTag("bar", "Hello, World!");
+            //    activity?.SetTag("baz", new int[] { 1, 2, 3 });
+            //}
+
+            using var openTelemetry = Sdk.CreateTracerProviderBuilder()
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("jaeger-test"))
+                    .AddSource("Samples.SampleClient", "Samples.SampleServer")
+                    .AddJaegerExporter(o =>
+                    {
+                        o.AgentHost = "localhost";
+                        o.AgentPort = 16686;
+                    })
+                    .Build();
+
+            // The above lines are required only in Applications which decide to use OpenTelemetry.
+
+            using (var sample = new InstrumentationWithActivitySource())
             {
-                activity?.SetTag("foo", 1);
-                activity?.SetTag("bar", "Hello, World!");
-                activity?.SetTag("baz", new int[] { 1, 2, 3 });
+                sample.Start();
+
+                Console.WriteLine("Traces are being created and exported" +
+                    "to Jaeger in the background. Use Jaeger to view them." +
+                    "Press ENTER to stop.");
+                Console.ReadLine();
             }
         }
     }
