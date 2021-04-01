038051345fc2bc3d5e862d5f4cd0d845d18ab237
Alex Vincent <alexv@microsoft.com>
2021-04-01 17:24:39 +0200

    changes

--- src/workerapp/Program.cs
+++ src/workerapp/Program.cs
@@ -7,14 +7,12 @@ namespace CasCap
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<WorkerService>();
-                });
+                })
+                .Build().Run();
+        }
     }
 }
